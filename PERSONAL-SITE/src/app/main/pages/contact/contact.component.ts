36e12c088e19963211fde55fe5540b10c3eada97
AnthonyMangano49 <32198206+AnthonyMangano49@users.noreply.github.com>
2017-10-16 01:30:59 -0400

    contact ts - were done for now

--- PERSONAL-SITE/src/app/main/pages/contact/contact.component.ts
+++ PERSONAL-SITE/src/app/main/pages/contact/contact.component.ts
@@ -1,1 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { TempApiService } from './temp-api.service';
 
+@Component({
+  selector: 'contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css'],
+  providers: [TempApiService]
+})
+export class ContactComponent implements OnInit {
+  form;
+  message;
+  canSubmit;
+
+  constructor(private tempApi: TempApiService) { }
+  ngOnInit() {
+    this.init();
+  }
+
+  init(){
+    this.form = {
+      category: '',
+      name: '',
+      message: '',
+      email: '',
+      phone: ''
+    }
+    this.message = 'Hello'
+    this.canSubmit = true;
+  }
+
+  submit(){
+    if(!this.canSubmit)
+      return;
+    if(this.form.email.length || this.form.phone.length){
+      this.canSubmit = false;
+      this.message = 'Thank You';
+      this.tempApi.sendEmail(this.form);
+      setTimeout(() => this.init(), 2000);
+    } else {
+      this.message = 'Phone or Email Required'
+    }
+  }
+
+}
